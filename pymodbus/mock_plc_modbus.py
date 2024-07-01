60946d11a958a69ac94bcb63751b4e3c7ec7dd6b
Marcus <88459939+marcuspavao@users.noreply.github.com>
2024-07-01 20:42:24 -0300

    Update mock_plc_modbus.py

--- pymodbus/mock_plc_modbus.py
+++ pymodbus/mock_plc_modbus.py
@@ -1,31 +1,39 @@
+from math import trunc
 import asyncio
-import random
 import time
 from pymodbus.client import AsyncModbusTcpClient
+import wmi
+from datetime import datetime
 
-#Comparação entre MODBUS TCP e MODBUS SERIAL
+w = wmi.WMI(namespace="root\OpenHardwareMonitor")
+
+def getTemperature():
+    temperature_infos = w.Sensor()
+    for sensor in temperature_infos:
+        if sensor.ProcessId == '6ebd23d0-be5a-4fb8-9090-675bfa53de57' and sensor.Parent == '/amdcpu/0' and sensor.InstanceId == '3914':
+            return trunc(sensor.Value * 100)
 
 async def write_data_to_server():
-    # IP address of the ESP32 Modbus server
     ESP32_IP_Address = '192.168.3.16'
 
-    # Create a Modbus TCP client\
-    client =  AsyncModbusTcpClient(
-        ESP32_IP_Address
-    )
-    
+    client = AsyncModbusTcpClient(ESP32_IP_Address)
     await client.connect()
 
     print("Client:", client)
 
     assert client.connected
 
-    for i in range(1000):
-        await client.write_register(101, random.randint(0, 100) ) #random.randint(0, 1000))
-        print(i)
-        time.sleep(20)
+    temperature = getTemperature()
 
-        #await client.write_register(202, i + 200) 
+    while True:
+        try:
+            temperature = getTemperature()
+            print(f"Temperature: {temperature}, Current time: {datetime.now().strftime('%H:%M:%S')}")
+            await client.write_register(101, temperature)
 
-# Run the asynchronous function
+            await asyncio.sleep(18)  # Aguarda 18 segundos antes de atualizar novamente
+        except Exception as e:
+            print(f"Erro ao comunicar com o servidor Modbus: {e}")
+            await asyncio.sleep(10)  # Tenta novamente após 10 segundos em caso de erro
+
 asyncio.run(write_data_to_server())
